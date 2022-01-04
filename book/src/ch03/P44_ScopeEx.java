3eaa6505b01c9ab771b19e09f87b253b9e133978
unknown <cesiya3@gmail.com>
2022-01-04 12:37:22 +0900

    2022/01/04 Java 교제예제

---
+++ book/src/ch03/P44_ScopeEx.java
@@ -0,0 +1,15 @@
+package ch03;
+
+public class P44_ScopeEx {  // 1. 클래스 블럭
+	
+	int no;  // 1-1. 클래스 블럭내에서 사용 가능한 변수
+	
+	public static void main(String[] args) {  // 2. main 메서드 블럭
+		
+		String name;  // 2-1. main 메서드 블럭내에서 사용 가능한 변수 
+		
+		name = "a";
+				
+		System.out.println(name);
+	}
+}
---
+++ book/src/ch04/p63.java
@@ -0,0 +1,18 @@
+package ch04;
+
+public class p63 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		
+		// &연산
+		System.out.println(a==b & test());
+		
+		// &&연산
+		System.out.println(a==b && test());
+		
+	public static boolean test() {	
+		System.out.println("");
+	}
+}
